696e2e770b989ea66ca7fde5d99553113888d735
Agus Kurniawan <agusk2007@gmail.com>
2014-09-11 16:45:20 +0700

    updated code

--- src/Job/IJob.cs
+++ src/Job/IJob.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Job
 {
-    interface IJob
+    public interface IJob
     {
+        string Perform(string jobId);
     }
 }
